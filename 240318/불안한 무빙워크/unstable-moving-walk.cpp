3a8f5a153de574faac1c52d6f8eb067d79f10f08
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2024-03-18 14:11:15 +0100

    Title: 불안한 무빙워크, Time: 43ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/불안한 무빙워크/unstable-moving-walk.cpp
+++ 240318/불안한 무빙워크/unstable-moving-walk.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
-#include <bitset>
+//#include <bitset>
 
 #define MAX_N 100
 using namespace std;
 
 int N, K;
 int safety[MAX_N * 2] = { 0, };
-bitset<MAX_N * 2> on;
+//bitset<MAX_N * 2> on;
+bool on[MAX_N * 2] = { 0, };
 int start, finish;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> N >> K;
     for (int i = 0; i < 2 * N; i++) cin >> safety[i];
-    on.reset();
+    //on.reset();
     start = 0, finish = N - 1;
-   
+
     int test = 0;
-    while(K > 0) {
-        
+    while (K > 0) {
+
         // 시작점 이동
         start = (start + 2 * N - 1) % (2 * N);
         finish = (finish + 2 * N - 1) % (2 * N);
-        if (on.test(finish)) on.set(finish, 0); 
-        
+        on[finish] = 0;
+
+
         //앞칸으로 전진 
         int i = (finish + 2 * N - 1) % (2 * N);
-        for (int cnt = 0; cnt < N-1; cnt++) {
-            if (on.test(i) == 0) continue;
-            
+        for (int cnt = 0; cnt < N - 1; cnt++, i = (i + 2 * N - 1) % (2 * N)) {
+            if (!on[i]) continue;
+
             // 다음 칸
             int j = (i + 1) % (2 * N);
-            if (safety[j] > 0 && on.test(j) == 0) {
-                on.set(i, 0);
-                on.set(j, 1);
+            if (safety[j] > 0 && !on[j]) {
+                on[i] = 0;
+                on[j] = 1;
                 safety[j]--;
                 if (!safety[j]) K--;
             }
-            i = (i + 2 * N - 1) % (2 * N);
+            
         }
-        //finish에 사람 있으면 빼줘.
-        if (on.test(finish)) on.set(finish, 0);
+        on[finish] = 0;
 
         //start에 사람 올리기
-        if (on.test(start) == 0 && safety[start] > 0) {
-            on.set(start, 1);
+        if (!on[start] && safety[start] > 0) {
+            on[start] = 1;
             safety[start]--;
             if (!safety[start]) K--;
         }
         //finish에 사람 있으면 빼줘.
-        if (on.test(finish)) on.set(finish, 0);
+        on[finish] = 0;
         test++;
     }
     cout << test;

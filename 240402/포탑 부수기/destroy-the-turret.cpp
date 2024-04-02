b65b4b0067af5838b444bcc05209095983b3fbe5
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2024-04-02 09:47:42 +0200

    Title: 포탑 부수기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240402/포탑 부수기/destroy-the-turret.cpp
+++ 240402/포탑 부수기/destroy-the-turret.cpp
@@ -1,172 +1,154 @@
 #include <iostream>
 #include <cstring>
 #include <queue>
-#include <tuple>
+#include <vector>
+#include <algorithm>
 #define MAX_N 10
 #define DIR_NUM 4
 using namespace std;
 
 int N, M;
 int grid_power[MAX_N][MAX_N];
-int grid_time[MAX_N][MAX_N] = {0,};
-int grid_dir[MAX_N][MAX_N];
-bool visited[MAX_N][MAX_N];
-bool is_attack[MAX_N][MAX_N];
+int grid_time[MAX_N][MAX_N] = { 0, };
+
+// 방향만 적으면 안됨. 범위 밖이면 순환되기 때문에 
+int back_y[MAX_N][MAX_N], back_x[MAX_N][MAX_N];
+bool vis[MAX_N][MAX_N];
+
 bool play = true;
-int turn = 1;
+int turn = 0;
 int dy[] = { 0,1,0,-1, -1,1,1,-1 }, dx[] = { 1,0,-1,0,1,1,-1,-1 };
 
-bool InRange(int y, int x) {
-    return 0 <= y && y < N && 0 <= x && x < M;
+struct node {
+    //2중 for, pair 로 비교하면 TLE
+    int y, x, t, p;
+};
+
+bool cmp(node a, node b) {
+    if (a.p != b.p) return a.p < b.p;
+    if (a.t != b.t) return a.t > b.t;//최근 공격한것 앞으로
+    if (a.y + a.x != b.y + b.x) return a.y + a.x > b.y + b.x;
+    return a.y > b.y;
 }
+vector<node> vec;
 
-pair<int, int> apos, dpos; // 공격,방어 위치 
-
-void FindTarget() {
-    pair<int, int> ainfo = { 987654321 ,0};//공격력,시간 
-    //시간에 음수 붙인다. 
-    for (int i = N - 1; i >= 0; i--) {
-        for (int j = M - 1; j >= 0; j--) {
-            if (!grid_power[i][j]) continue;
-            if (make_pair(grid_power[i][j], -grid_time[i][j]) < ainfo) {
-                ainfo = { grid_power[i][j], -grid_time[i][j] };
-                apos = { i,j };
-            }
-        }
-    }
-    grid_time[apos.first][apos.second] = turn;
-    grid_power[apos.first][apos.second] += N + M;
-
-    pair<int, int> dinfo = {0,0};//공격력,시간 
+void FindAttacker() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            if (!grid_power[i][j]) continue;
-            if (dinfo < make_pair(grid_power[i][j], -grid_time[i][j])) {
-                dinfo = { grid_power[i][j], -grid_time[i][j] };
-                dpos = { i,j };
-            }
+            if (grid_power[i][j] <= 0) continue;
+            vec.push_back({ i,j,grid_time[i][j],grid_power[i][j] });
         }
     }
+    sort(vec.begin(), vec.end(), cmp);
+    int y = vec[0].y, x = vec[0].x;
+    vec[0].p += N + M;
+    vec[0].t = turn;
+    grid_power[y][x] += N + M;
+    grid_time[y][x] = turn;
 }
 
 bool LaserAttack() {
+    // 시작 ,끝 위치 
+    int sy = vec[0].y, sx = vec[0].x;
+    int ey = vec.back().y, ex = vec.back().x;
+
     queue<pair<int, int> > q;
-    q.push(apos);
-    visited[apos.first][apos.second] = true;
+    q.push({ sy,sx });
+    vis[sy][sx] = true;
     while (!q.empty()) {
-        pair<int, int> cur = q.front(); q.pop();
-        if (cur == dpos) break;
+        int y = q.front().first, x = q.front().second;
+        q.pop();
+
+        if (ey == y && ex == x) break;
 
         for (int d = 0; d < DIR_NUM; d++) {
-            int ny = (cur.first + dy[d]) % N;
-            int nx = (cur.second + dx[d]) % M;
-            if (visited[ny][nx] || !grid_power[ny][nx]) continue;
-            visited[ny][nx] = true;
+            int ny = (y + dy[d] + N) % N;
+            int nx = (x + dx[d] + M) % M;
+            if (vis[ny][nx] || !grid_power[ny][nx]) continue;
+            vis[ny][nx] = true;
             q.push({ ny,nx });
-            grid_dir[ny][nx] = d;
+            back_y[ny][nx] = y;
+            back_x[ny][nx] = x;
         }
     }
 
-    int y, x;
-    tie(y, x) = dpos;
-    
-    if(!visited[y][x])return false;
-  
-    memset(visited, 0, sizeof(visited));
-    visited[y][x] = true;
-    int attack_power = grid_power[apos.first][apos.second];
-    grid_power[y][x] -= attack_power;
+    if (!vis[ey][ex])return false;
+
+    memset(vis, 0, sizeof(vis));
+    vis[sy][sx] = vis[ey][ex] = true;
+    int attack_power = grid_power[sy][sx];
+    grid_power[ey][ex] -= attack_power;
+    if (grid_power[ey][ex] < 0) grid_power[ey][ex] = 0;
     attack_power /= 2;
 
     while (1) {
-        //다음 위치
-        int dir = (grid_dir[y][x] + 2) % DIR_NUM;
-        y += dy[dir], x += dx[dir];
-        visited[y][x] = true;
-        if (apos == make_pair(y, x)) break;
-        grid_power[y][x] -= attack_power;
+        int ny = back_y[ey][ex], nx = back_x[ey][ex];
+        vis[ny][nx] = true;
+        if (sy == ny && sx == nx) break;
+        grid_power[ny][nx] -= attack_power;
+        if (grid_power[ny][nx] < 0) grid_power[ny][nx] = 0;
+        ey = ny, ex = nx;
     }
     return true;
 }
 void BombAttack() {
-    int y, x;
-    tie(y, x) = dpos;
-    visited[y][x] = true;
-    visited[apos.first][apos.second] = true;
-    int attack_power = grid_power[apos.first][apos.second];
-    grid_power[y][x] -= attack_power;
+    int sy = vec[0].y, sx = vec[0].x;
+    int ey = vec.back().y, ex = vec.back().x;
+    vis[sy][sx] = vis[ey][ex] = true;
+
+    int attack_power = grid_power[sy][sx];
+    grid_power[ey][ex] -= attack_power;
+    if (grid_power[ey][ex] < 0) grid_power[ey][ex] = 0;
     attack_power /= 2;
     for (int d = 0; d < 8; d++) {
-        int ny = (y + dy[d]) % N, nx = (x + dx[d]) % M;
-        visited[ny][nx] = true;
+        int ny = (ey + dy[d] + N) % N, nx = (ex + dx[d] + M) % M;
+        vis[ny][nx] = true;
         grid_power[ny][nx] -= attack_power;
+        if (grid_power[ny][nx] < 0) grid_power[ny][nx] = 0;
     }
-}
-
-bool End() {
-    int cnt = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (grid_power[i][j] > 0) cnt++;
-            else grid_power[i][j] = 0;
-            if (cnt >= 2) return false;
-        }
-    }
-    return true;
 }
 
 void AddPower() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            if (grid_power[i][j] > 0 && !visited[i][j]) {
+            if (grid_power[i][j] > 0 && !vis[i][j]) {
                 grid_power[i][j]++;
             }
         }
     }
 }
 
-void Simulate() {
-    //대상 선정
-    FindTarget();
-    //위치 값 어떻게 넘겨줄지 
-
-    //공격
-    memset(visited, 0, sizeof(visited));
-    memset(grid_dir, -1, sizeof(grid_dir));
-    if (!LaserAttack()) {
-        memset(visited, 0, sizeof(visited));
-        BombAttack();
-    }
-        
-
-    if (End()) {
-        play = false;
-        return;
-    }
-
-    //점수 추가 
-    AddPower();
-
-}
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    //pair<int, int> a = { 1,3 }, b = { 1,3 };
-    //if (b < a) cout << "바귐";
-
     int K;
     cin >> N >> M >> K;
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
             cin >> grid_power[i][j];
-    while (turn <= K && play) {
-        Simulate();
+    while (K--) {
+        vec.clear();
+        memset(vis, 0, sizeof(vis));
+        memset(back_y, 0, sizeof(back_y));
+        memset(back_x, 0, sizeof(back_x));
         turn++;
+
+        //step1
+        FindAttacker();
+        if (vec.size() <= 1) break;
+
+        if (!LaserAttack()) {
+            memset(vis, 0, sizeof(vis));
+            BombAttack();
+        }
+
+        //점수 추가 
+        AddPower();
+
     }
     int ret = 0;
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
-            if (ret < grid_power[i][j]) ret = grid_power[i][j];
+            ret = max(ret, grid_power[i][j]);
     cout << ret;
     return 0;
 }

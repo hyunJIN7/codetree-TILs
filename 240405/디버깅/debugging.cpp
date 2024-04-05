8bc1819f7da075aedf418bb67158a42ef1879809
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2024-04-05 08:07:50 +0200

    Title: 디버깅, Time: 380ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/디버깅/debugging.cpp
+++ 240405/디버깅/debugging.cpp
@@ -34,12 +34,14 @@ bool Verify() {
     return true;
 }
 
-vector<int> list;
 
 void FindMin(int start,int cnt) {
     if (cnt > ans) return;
     if (Verify()) 
         ans = min(ans, cnt);
+    
+    
+    // 1번 
     if (cnt > 3 || start == candi.size()) return;
 
     FindMin(start + 1, cnt);
@@ -49,26 +51,20 @@ void FindMin(int start,int cnt) {
     //선 겹침 
     if (InRange(x - 1) && grid[y][x - 1]) return;
     if (InRange(x + 1) && grid[y][x + 1]) return;
-    list.push_back(start);
     grid[y][x] = true;
     FindMin(start +1, cnt + 1);
-    list.pop_back();
     grid[y][x] = false;
 
+    //// 2번 더 느림
     //for (int i = start; i < candi.size(); i++) {
     //    int y, x;
     //    tie(y, x) = candi[i];
-
     //    //선 겹침 
     //    if (InRange(x - 1) && grid[y][x - 1]) continue;
     //    if (InRange(x + 1) && grid[y][x + 1]) continue;
-
-    //    list.push_back(i);
     //    grid[y][x] = true;
     //    FindMin(i +1, cnt + 1);
-    //    list.pop_back();
     //    grid[y][x] = false;
-    //   
     //}
 }
 

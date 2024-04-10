454f7bad99cb47565dfc16edd1da37f738f14aa7
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2024-04-10 15:23:31 +0200

    Title: 꼬리잡기놀이, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240410/꼬리잡기놀이/tail-catch-play.cpp
+++ 240410/꼬리잡기놀이/tail-catch-play.cpp
@@ -1,216 +1,185 @@
 #include <iostream>
+#include <tuple>
 #include <vector>
 
 #define MAX_N 20
 #define MAX_M 5
+#define WAY 4
+#define HEAD 1
+#define TAIL 3
+#define MID 2
 #define DIR_NUM 4
-
 using namespace std;
+typedef pair<int, int> pii;
 
-int n, m, k;
-int board[MAX_N + 1][MAX_N + 1];
+int N, M, K;
 
-// 각 팀별 레일 위치를 관리합니다.
-vector<pair<int, int> > v[MAX_M + 1];
-// 각 팀별 tail 위치를 관리합니다.
-int tail[MAX_M + 1];
-bool visited[MAX_N + 1][MAX_N + 1];
+int grid[MAX_N][MAX_N];
+int grid_id[MAX_N][MAX_N];
 
-// 격자 내 레일에 각 팀 번호를 적어줍니다.
-int board_idx[MAX_N + 1][MAX_N + 1];
+vector<pii> line[MAX_M + 1];//그룹별 라인 관리
+int tail[MAX_N + 1];//line에서 꼬리 몇번째에 있는지
 
-int ans;
+int score = 0;
 
-int dx[DIR_NUM] = { -1, 0, 1, 0 };
-int dy[DIR_NUM] = { 0, -1, 0, 1 };
+int dy[DIR_NUM] = { 0,0,1,-1 };
+int dx[DIR_NUM] = { 1,-1,0,0 };
+bool vis[MAX_N][MAX_N];
 
-bool IsOutRange(int x, int y) {
-    return !(1 <= x && x <= n && 1 <= y && y <= n);
+bool InRange(int y, int x) {
+    return 0 <= y && y < N && 0 <= x && x < N;
 }
 
-// 초기 레일을 만들기 위해 dfs를 이용합니다.
-void DFS(int x, int y, int idx) {
-    visited[x][y] = true;
-    board_idx[x][y] = idx;
-    for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (IsOutRange(nx, ny)) continue;
+void MakeGroup(int y,int x, int id) {
+    //y,x 다음 위치 찾기
+    grid_id[y][x] = id;
+    vis[y][x] = true;
+    for (int d = 0; d < DIR_NUM; d++) {
+        int ny = y + dy[d], nx = x + dx[d];
+        if (!InRange(ny, nx) || vis[ny][nx] || grid[ny][nx]==0) continue;
 
-        // 이미 지나간 경로거나 경로가 아니면 넘어갑니다.
-        if (board[nx][ny] == 0) continue;
-        if (visited[nx][ny]) continue;
+        //몸통 방향으로 넣어야하니까
+        if (grid[ny][nx] != MID && line[id].size() == 1) continue;
 
-        // 가장 처음 탐색할 때 2가 있는 방향으로 dfs를 진행합니다.
-        if ((int)v[idx].size() == 1 && board[nx][ny] != 2) continue;
+        //여기 오면
+        //몸통/꼬리/길 순차적으로 넣어진다.
+        line[id].push_back({ ny,nx });
+        if (grid[ny][nx] == TAIL) tail[id] = line[id].size() - 1;
 
-        v[idx].push_back({ nx, ny });
-        if (board[nx][ny] == 3) tail[idx] = (int)v[idx].size();
-        DFS(nx, ny, idx);
+        //다음 위치 찾았으면 다시 진행
+        //이 함수는 하나 그룹 길 다 찾을 때 까지 진행됨.
+        // line[id]에 하나의 길 정보 위치 다 들어감
+        MakeGroup(ny, nx, id);
     }
 }
 
-// 입력을 받는 등 초기 작업을 합니다.
-void Init() {
-    cin >> n >> m >> k;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            cin >> board[i][j];
-
-    int cnt = 1;
-
-    // 레일을 벡터에 저장합니다. 머리사람을 우선 앞에 넣어줍니다.
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (board[i][j] == 1) v[cnt++].push_back({ i, j });
-
-    // dfs를 통해 레일을 벡터에 순서대로 넣어줍니다.
-    for (int i = 1; i <= m; i++)
-        DFS(v[i][0].first, v[i][0].second, i);
+void RenewGrid(int id) {
+    int size = line[id].size();
+    //line에 순서대로 grid 에 반영
+    for (int i = 0; i < size; i++) {
+        int y, x;
+        tie(y, x) = line[id][i];
+        if (i == 0) {
+            grid[y][x] = HEAD;
+        }
+        else if (i < tail[id]) {
+            grid[y][x] = MID;
+        }
+        else if (i == tail[id]) {
+            grid[y][x] = MID;
+        }
+        else {
+            grid[y][x] = WAY;
+        }
+    }
 }
 
-// 각 팀을 이동시키는 함수입니다.
 void MoveAll() {
-    for (int i = 1; i <= m; i++) {
-        // 각 팀에 대해 레일을 한 칸씩 뒤로 이동시킵니다.
-        pair<int, int> tmp = v[i].back();
-        for (int j = (int)v[i].size() - 1; j >= 1; j--)
-            v[i][j] = v[i][j - 1];
-        v[i][0] = tmp;
-    }
 
-    for (int i = 1; i <= m; i++) {
-        // 벡터에 저장한 정보를 바탕으로 보드의 표기 역시 바꿔줍니다.
-        for (int j = 0; j < (int)v[i].size(); j++) {
-            pair<int, int> x = v[i][j];
-            if (j == 0)
-                board[x.first][x.second] = 1;
-            else if (j < tail[i] - 1)
-                board[x.first][x.second] = 2;
-            else if (j == tail[i] - 1)
-                board[x.first][x.second] = 3;
-            else
-                board[x.first][x.second] = 4;
+    for (int id = 1; id <= M; id++) {
+        pii tmp = line[id].back();
+        for (int i = line[id].size()-1; i > 0; i--) {
+            line[id][i] = line[id][i - 1];//하나씩 땡겨
+        }
+        line[id][0] = tmp;
+
+        RenewGrid(id);
+     
+    }
+}
+
+void GetScore(int y, int x) {
+    int id = grid_id[y][x];
+    pii target = { y,x };
+    for (int i = 0; i < line[id].size(); i++) {
+        if (target == line[id][i]) {
+            score += (i + 1) * (i + 1);
+            return;
         }
     }
 }
 
-// (x, y) 지점에 공이 닿았을 때의 점수를 계산합니다.
-void GetPoint(int x, int y) {
-    int idx = board_idx[x][y];
-    int cnt = 0;
-    for (int i = 0; i < (int)v[idx].size(); i++)
-        if (v[idx][i].first == x && v[idx][i].second == y) cnt = i;
-    ans += (cnt + 1) * (cnt + 1);
+void Reverse(int id) {
+    int size = line[id].size();
+    int s = tail[id];
+    vector<pii> newv;
+    for (int i = 0; i < size; i++) {
+        newv.push_back(line[id][(s - i + size) % size]);
+    }
+    line[id] = newv;
+    RenewGrid(id);
 }
 
-// turn 번째 라운드의 공을 던집니다.
-// 공을 던졌을 때 이를 받은 팀의 번호를 반환합니다.
 int ThrowBall(int turn) {
-    int t = (turn - 1) % (4 * n) + 1;
+    int t = turn % (4*N);
+    int ret = 0;
+    if (t < N) {
+        t %= N;
+        for (int i = 0; i < N; i++) {
+            if (HEAD <= grid[t][i] && grid[t][i] <= TAIL) {
+                GetScore(t, i);
+                return grid_id[t][i];
+            }
+        }
 
-    if (t <= n) {
-        // 1 ~ n 라운드의 경우 왼쪽에서 오른쪽 방향으로 공을 던집니다.
-        for (int i = 1; i <= n; i++) {
-            if (1 <= board[t][i] && board[t][i] <= 3) {
-                // 사람이 있는 첫 번째 지점을 찾습니다.
-                // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
-                GetPoint(t, i);
-                return board_idx[t][i];
+    }
+    else if (t < 2 * N) {
+        t %= N;
+        for (int i = N-1; i >= 0; i--) {
+            if (HEAD <= grid[i][t] && grid[i][t] <= TAIL) {
+                GetScore(i, t);
+                return grid_id[i][t];
             }
         }
     }
-    else if (t <= 2 * n) {
-        // n+1 ~ 2n 라운드의 경우 아래에서 윗쪽 방향으로 공을 던집니다.
-        t -= n;
-        for (int i = 1; i <= n; i++) {
-            if (1 <= board[n + 1 - i][t] && board[n + 1 - i][t] <= 3) {
-                // 사람이 있는 첫 번째 지점을 찾습니다.
-                // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
-                GetPoint(n + 1 - i, t);
-                return board_idx[n + 1 - i][t];
+    else if (t < 3 * N) {
+        t %= N;
+        for (int i = N - 1; i >= 0; i--) {
+            if (HEAD <= grid[N - 1 - t][i] && grid[N - 1 - t][i] <= TAIL) {
+                GetScore(N - 1 - t, i);
+                return grid_id[N - 1 - t][i];
             }
         }
     }
-    else if (t <= 3 * n) {
-        // 2n+1 ~ 3n 라운드의 경우 오른쪽에서 왼쪽 방향으로 공을 던집니다.
-        t -= (2 * n);
-        for (int i = 1; i <= n; i++) {
-            if (1 <= board[n + 1 - t][n + 1 - i] && board[n + 1 - t][n + 1 - i] <= 3) {
-                // 사람이 있는 첫 번째 지점을 찾습니다.
-                // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
-                GetPoint(n + 1 - t, n + 1 - i);
-                return board_idx[n + 1 - t][n + 1 - i];
+    else{
+        t %= N;
+        for (int i = 0; i < N; i++) {
+            if (HEAD <= grid[i][N - 1 - t] && grid[i][N - 1 - t] <= TAIL) {
+                GetScore(i, N - 1 - t);
+                return grid_id[i][N - 1 - t];
             }
         }
     }
-    else {
-        // 3n+1 ~ 4n 라운드의 경우 위에서 아랫쪽 방향으로 공을 던집니다.
-        t -= (3 * n);
-        for (int i = 1; i <= n; i++) {
-            if (1 <= board[i][n + 1 - t] && board[i][n + 1 - t] <= 3) {
-                // 사람이 있는 첫 번째 지점을 찾습니다.
-                // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
-                GetPoint(i, n + 1 - t);
-                return board_idx[i][n + 1 - t];
-            }
-        }
-    }
-    // 공이 그대로 지나간다면 0을 반환합니다.
     return 0;
 }
 
-// 공을 획득한 팀을 순서를 바꿉니다.
-void Reverse(int got_ball_idx) {
-    // 아무도 공을 받지 못했으면 넘어갑니다.
-    if (got_ball_idx == 0) return;
 
-    int idx = got_ball_idx;
-
-    vector<pair<int, int> > new_v;
-
-    // 순서를 맞춰 new_v에 레일을 넣어줍니다.
-    for (int i = tail[idx] - 1; i >= 0; i--) {
-        pair<int, int> x = v[idx][i];
-        new_v.push_back(x);
+void Init() {
+    cin >> N >> M >> K;
+    for (int i = 0, g = 1; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> grid[i][j];
+            if (grid[i][j] == HEAD) {
+                line[g++].push_back({ i,j });
+            }
+        }
     }
 
-    for (int i = (int)v[idx].size() - 1; i >= tail[idx]; i--) {
-        pair<int, int> x = v[idx][i];
-        new_v.push_back(x);
+    for (int id = 1; id <= M; id++) {
+        MakeGroup(line[id][0].first, line[id][0].second, id);
     }
 
-    v[idx] = new_v;
-
-    // 벡터에 저장한 정보를 바탕으로 보드의 표기 역시 바꿔줍니다.
-    for (int j = 0; j < (int)v[idx].size(); j++) {
-        pair<int, int> x = v[idx][j];
-        if (j == 0)
-            board[x.first][x.second] = 1;
-        else if (j < tail[idx] - 1)
-            board[x.first][x.second] = 2;
-        else if (j == tail[idx] - 1)
-            board[x.first][x.second] = 3;
-        else
-            board[x.first][x.second] = 4;
-    }
 }
-
 int main() {
-    // 입력을 받고 구현을 위한 기본적인 처리를 합니다.
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     Init();
-
-    for (int i = 1; i <= k; i++) {
-        // 각 팀을 머리사람을 따라 한칸씩 이동시킵니다.
+    for (int turn = 0; turn < K; turn++) {
         MoveAll();
-
-        // i번째 라운드의 공을 던집니다. 공을 받은 사람을 찾아 점수를 추가합니다.
-        int got_ball_idx = ThrowBall(i);
-
-        // 공을 획득한 팀의 방향을 바꿉니다.
-        Reverse(got_ball_idx);
+        int id = ThrowBall(turn);
+        if (id > 0) {
+            Reverse(id);
+        }
     }
-
-    cout << ans;
+    cout << score;
     return 0;
 }
